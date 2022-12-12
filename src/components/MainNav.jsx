e911938542a172fdc8a2a4b5b1a130c5b07b5348
Ben Bushell <ben.bushell87@icloud.com>
2022-12-12 17:08:11 +0700

    add navigation component

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+export const MainNav = () => {
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+  };
+
+  return (
+    <nav className="main-nav">
+      <ul className="nav-list">
+        <NavLink
+          to="/phrases"
+          className="nav-link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Phrases
+        </NavLink>
+        <NavLink
+          to="/flashcards"
+          className="nav-link"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Flashcards
+        </NavLink>
+      </ul>
+    </nav>
+  );
+};
